59ab5b1dd0925efb9c0cdb5446959e4b1d0588e0
Vlad Popov <vlad.linkoln.94@gmail.com>
2018-10-29 09:14:26 +0200

    change ie responsive ti Object.entries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+if (!Object.entries) {
+    Object.entries = function( obj ){
+        var ownProps = Object.keys( obj ),
+            i = ownProps.length,
+            resArray = new Array(i); // preallocate the Array
+        while (i--)
+            resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+        return resArray;
+    };
+}
+
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import BookingPage from "./components/BookingPage/";
